8887faa11cf7f45493854aa831fd49d3904b3882
arshiazt <arshiazabeti@gmail.com>
2026-07-12 00:02:28 +0330

    implement dynamic URLs for website app pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import signup_view,login_view
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('login/',login_view,name='login'),
     path('signup/',signup_view,name='signup'),
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import blog_view,blog_single_view
 
+app_name = 'blog'
+
 urlpatterns = [
     path('',blog_view,name='index'),
     path('single',blog_single_view,name='single'),
