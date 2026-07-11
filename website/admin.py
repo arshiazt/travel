7dbe5f688dd945e7ec147257166f0132eb0717b3
arshiazt <arshiazabeti@gmail.com>
2026-07-12 00:11:38 +0330

    create Contact model & Contact admin

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    
+    list_display = ('name','email','created_date')
+    list_filter = ('email',)
+    search_fields = ('name','message')
+
+admin.site.register(Contact,ContactAdmin)
