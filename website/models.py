f3b6fa515fce78358cd92c98fb43fbf98a0642c2
arshiazt <arshiazabeti@gmail.com>
2026-07-12 00:54:53 +0330

    edit contact model & admin

--- website/models.py
+++ website/models.py
@@ -9,5 +9,18 @@ class Contact(models.Model):
     subject = models.CharField(max_length=255)
     message = models.TextField()
 
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return self.name
+
+class Newsletter(models.Model):
+    
+    email = models.EmailField()
+
     def __str__(self):
         return self.email
