179359c16cbc1ccc2e08d01f450dc7c48cd9099b
arshiazt <arshiazabeti@gmail.com>
2026-07-12 05:44:15 +0330

    complete the accounts app authentication system

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth  import authenticate,login,logout
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def signup_view(request):
-    return render(request,'accounts/signup.html')
+
+    if request.user.is_authenticated:
+        return redirect('/')
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/login')
+        
+    form = UserCreationForm()
+    context = {'form':form}
+    return render(request,'accounts/signup.html',context)
 
 def login_view(request):
-    return render(request,'accounts/login.html')
+    
+    if request.user.is_authenticated:
+        return redirect('/')
+    
+    if request.method == 'POST':
+        
+        form = AuthenticationForm(request=request,data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('/')
+        
+    form = AuthenticationForm()
+    context = {'form':form}
+    return render(request,'accounts/login.html',context)
+    
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('/')
